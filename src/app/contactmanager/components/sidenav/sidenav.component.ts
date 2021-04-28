cefda816c3668a3328ea08174dc21889535e7a25
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-04-28 23:12:44 +0900

    adding responsivensess

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,15 +1,24 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 
+const SMALL_WIDTH_BREAKPOINT = 720;
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  isScreenSmall = true;
 
-  constructor() { }
+  constructor(private breakPointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.breakPointObserver
+      // .observe([Breakpoints.XSmall]);
+      .observe([`(max-width: ${SMALL_WIDTH_BREAKPOINT}px)`])
+      .subscribe(state => {
+        this.isScreenSmall = state.matches;
+      });
   }
 
 }
