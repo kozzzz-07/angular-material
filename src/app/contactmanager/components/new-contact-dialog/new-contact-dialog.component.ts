aa412e66a575800cf8670b356a129e7ca2eda7f8
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-04-30 00:06:34 +0900

    customizing the form

--- src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contactmanager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-new-contact-dialog',
@@ -6,18 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-contact-dialog.component.scss']
 })
 export class NewContactDialogComponent implements OnInit {
+  user: User = {
+    id: 0,
+    birthDate: new Date(),
+    name: '',
+    avatar: '',
+    bio: '',
+    notes: [],
+  };
 
-  constructor() { }
+  avatars = ['svg-1', 'svg-2', 'svg-3', 'svg-4']
+
+  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
 
   ngOnInit(): void {
   }
 
   save(): void {
-
+    this.dialogRef.close(this.user);
   }
 
   dismiss(): void {
-    
+    this.dialogRef.close(null);
   }
 
 }
